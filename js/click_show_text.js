ec305d6b014b566902f05bf68565cbae5e481418
YCZKL <yuchao1076244056@163.com>
2019-08-02 14:17:42 +0800

    Site updated: 2019-08-02 14:17:41

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        ("喻超&朱凯琳", "喻超&朱凯琳", "喻超&朱凯琳", "喻超&朱凯琳","喻超&朱凯琳", "喻超&朱凯琳", "喻超&朱凯琳","喻超&朱凯琳");
+        ("喻超❤朱凯琳", "喻超❤朱凯琳", "喻超❤朱凯琳", "喻超❤朱凯琳","喻超❤朱凯琳", "喻超❤朱凯琳", "喻超❤朱凯琳","喻超❤朱凯琳");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
