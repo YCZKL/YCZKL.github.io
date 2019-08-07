4c6423f593e1484a504b43bb9b31779829afdb32
YCZKL <yuchao1076244056@163.com>
2019-08-07 12:48:15 +0800

    Site updated: 2019-08-07 12:48:14

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -9,7 +9,7 @@
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(???3)ノ欢迎帅的人回来！' + OriginTitle;
+         document.title = 'ヾ(???3)ノ欢迎美的人回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
